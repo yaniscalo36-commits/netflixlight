1b761040d0c12df380861f5a5c8007acba7123d1
HaxTwoWater <louis.jeannot@ynov.com>
2026-03-19 14:12:08 +0100

    feat(server): modified the server.js on the server branchs to update it and add new features in the futur

--- server.js
+++ server.js
@@ -1,12 +1,21 @@
 const express = require("express");
+const dotenv = require("dotenv").config();
+const usersRoutes = require("./src/routes/users.routes");
+const path = require("path");
+
 const app = express();
+const PORT = 3000;
 
 app.use(express.json());
 
+app.use(express.static(path.join(__dirname, "web", "static")));
+
+app.use("/src/routes", usersRoutes);
+
 app.get("/", (req, res) => {
-  res.send("Serveur Node.js OK");
+  res.sendFile(path.join(__dirname, "web", "templates", "index.html"));
 });
 
-app.listen(3000, () => {
-  console.log("Serveur lancé sur http://localhost:3000");
+app.listen(PORT, () => {
+  console.log(`Serveur lancé sur http://localhost:${PORT}`);
 });
