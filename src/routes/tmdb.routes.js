864d24d519f60d7c51e5667474567bf5e7b93999
HaxTwoWater <louis.jeannot@ynov.com>
2026-04-10 15:26:57 +0200

    fix(routes): removed the tdmb routes that was here to test the routes for the api relations

--- src/routes/tmdb.routes.js
+++
@@ -1,8 +0,0 @@
-const express = require("express");
-const router = express.Router();
-const tmdbController = require("../controllers/tmdb.controller");
-
-router.get("/test", tmdbController.testMovie);
-router.get("/search", tmdbController.searchMovies);
-
-module.exports = router;
