d61159ec37414dc9846cd190191dc016d5880ab7
HaxTwoWater <louis.jeannot@ynov.com>
2026-04-10 12:33:04 +0200

    fix(server): fixedd some issue related to some require and path to be able making our backend the api communicating

--- server.js
+++ server.js
@@ -1,16 +1,19 @@
+require("dotenv").config();
+
 const express = require("express");
-const dotenv = require("dotenv").config();
-const usersRoutes = require("./src/routes/users.routes");
 const path = require("path");
+
+const usersRoutes = require("./src/routes/users.routes");
+const moviesRoutes = require("./src/routes/movies.routes");
 
 const app = express();
 const PORT = 3000;
 
 app.use(express.json());
-
 app.use(express.static(path.join(__dirname, "web", "static")));
 
-app.use("/src/routes", usersRoutes);
+app.use("/api/users", usersRoutes);
+app.use("/api/movies", moviesRoutes);
 
 app.get("/", (req, res) => {
   res.sendFile(path.join(__dirname, "web", "templates", "index.html"));
