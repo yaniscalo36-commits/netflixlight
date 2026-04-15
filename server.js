942a563b937de6d7b512e8fc7255cd2a8294a165
HaxTwoWater <louis.jeannot@ynov.com>
2026-04-15 20:19:54 +0200

    fix(server): hotfix for the index.html that wasn't returning the index.html pages also some few issues of syntax about ';'

--- server.js
+++ server.js
@@ -18,18 +18,21 @@ app.use("/api/movies", moviesRoutes);
 app.get("/", (req, res) => {
   res.sendFile(path.join(__dirname, "web", "templates", "index.html"));
 });
+app.get("/index.html", (req, res) => {
+  res.sendFile(path.join(__dirname, "web", "templates", "index.html"));
+});
 
 app.get("/favoris.html", (req, res) => {
   res.sendFile(path.join(__dirname, "web", "templates", "favoris.html"));
-})
+});
 
 app.get("/film.html", (req, res) => {
   res.sendFile(path.join(__dirname, "web", "templates", "film.html"));
-})
+});
 
 app.get("/lecteur.html", (req, res) => {
   res.sendFile(path.join(__dirname, "web", "templates", "lecteur.html"));
-})
+});
 
 app.get("/authentification.html", (req, res) => {
   res.sendFile(path.join(__dirname, "web", "templates", "login.html"));
